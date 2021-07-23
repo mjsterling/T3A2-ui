8f756bce938a971358bd51f30b7ad8e9012e19e4
mjsterling <mjsterling93@gmail.com>
2021-07-23 17:57:55 +1000

    deleted unnecessary partials

--- src/components/partials/SocialLinks.tsx
+++
@@ -1,5 +0,0 @@
-import { Grid } from "@material-ui/core";
-
-export default function SocialLinks() {
-  return <Grid container></Grid>;
-}
--- src/components/partials/index.ts
+++ src/components/partials/index.ts
@@ -2,24 +2,20 @@ import Admin from "./Admin/index";
 import BookingPopup from "./BookingPopup";
 import DateSelector from "./DateSelector";
 import ImageCarousel from "./ImageCarousel";
-import ImagePopup from "./ImagePopup";
 import Logo from "./Logo";
 import PageFooter from "./PageFooter";
 import PaxSelector from "./PaxSelector";
 import RoomCard from "./RoomCard";
-import SocialLinks from "./SocialLinks";
 
 const _ = {
   Admin,
   BookingPopup,
   DateSelector,
   ImageCarousel,
-  ImagePopup,
   Logo,
   PageFooter,
   PaxSelector,
   RoomCard,
-  SocialLinks,
 };
 
 export default _;
